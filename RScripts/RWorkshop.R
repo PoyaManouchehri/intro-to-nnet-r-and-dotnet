24fff54e242475635443983965b30950481371d0
Poya Manouchehri <poya@manouchehri.net>
2016-09-15 22:16:22 +1000

    Updating workshop script

--- RScripts/RWorkshop.R
+++ RScripts/RWorkshop.R
@@ -1,12 +1,12 @@
 loadBankMarketingData <- function()
 {
-  bankData <- read.csv("C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/Data/BankMarketing.csv")
+  bankData <- read.csv("C:/RWorkshop/BankMarketing.csv")
   numberCols <- sapply(bankData, function(x) {return(is.numeric(x) | is.integer(x))})
   means <- lapply(bankData[numberCols], mean)
   sds <- lapply(bankData[numberCols], sd)
   bankData[numberCols] <- (bankData[numberCols] - means)/sds
-  saveRDS(means, file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/Means')
-  saveRDS(sds, file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/SDs')
+  saveRDS(means, file='C:/RWorkshop/Means.bin')
+  saveRDS(sds, file='C:/RWorkshop/SDs.bin')
   return(bankData)
 }
 
@@ -14,17 +14,14 @@ trainBankModel <- function()
 {
   bankData <- loadBankMarketingData()
   set.seed(1)
-  
   testIndices <- sample(nrow(bankData), 900)
   testSet <- bankData[testIndices,]
   trainingSet <- bankData[-testIndices,]
   
   bankModel <- nnet(Y~., trainingSet, size=10, MaxNWts=5000, maxit=2000)
   testBankModel(bankModel, testSet)
-
-  saveRDS(bankModel, file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/MyModel')
-  
-  return (bankModel)
+  saveRDS(bankModel, file='C:/RWorkshop/BankModel.bin')
+  return(bankModel)
 }
 
 testBankModel <- function(bankModel, testSet)
@@ -37,6 +34,7 @@ testBankModel <- function(bankModel, testSet)
   noTotal <- total - yesTotal
 
   matches <- testPredictions[which(testPredictions == testSet[, outputCol])]
+
   correctTotal <- length(matches)
   correctYes <- length(which(matches == c("yes")))
   correctNo <- correctTotal - correctYes
@@ -48,12 +46,14 @@ testBankModel <- function(bankModel, testSet)
 
 makePrediction <- function(input)
 {
-  bankModel <- readRDS(file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/MyModel')
-  means <- readRDS(file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/Means')
-  sds <- readRDS(file='C:/Dev/Readify/Presentations/intro-to-nnet-r-and-dotnet/RScripts/SDs')
+  bankModel <- readRDS(file='C:/RWorkshop/BankModel.bin')
+  means <- readRDS(file='C:/RWorkshop/Means.bin')
+  sds <- readRDS(file='C:/RWorkshop/SDs.bin')
+
   numberCols <- sapply(input, function(x) {return(is.numeric(x) | is.integer(x))})
   input[numberCols] = (input[numberCols] - means) / sds
+
   result <- predict(bankModel, input, type="class")
-  
   return(result[1])
 }
+
